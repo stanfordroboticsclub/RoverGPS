848fef83ceb6a48d4b2a6307e878d08f451afd65
Michal Adamkiewicz <mikh3x4@gmail.com>
2018-11-29 03:33:51 -0800

    install.sh installs service files now

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env sh
 
+FOLDER=$(dirname $(realpath "$0"))
+cd $FOLDER
+
 pip3 install adafruit-circuitpython-gps
+
+for file in *.service; do
+    [ -f "$file" ] || break
+    sudo ln -s $FOLDER/$file /lib/systemd/system/
+done
