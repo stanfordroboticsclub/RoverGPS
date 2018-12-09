ee21be59aca64403f5d8a0b88770831174413457
Michal Adamkiewicz <mikh3x4@gmail.com>
2018-12-08 16:41:45 -0800

    independant rtcm module

--- rtcm_slac.py
+++ rtcm_slac.py
@@ -6,6 +6,8 @@ from base64 import b64encode
 import serial, pynmea2, time
 from user_pass import user
 
+publish_interval = 1
+
 server ="rtgpsout.unavco.org:2101"
 
 headers = {
@@ -19,6 +21,8 @@ connection = HTTPConnection(server)
 connection.request('GET', '/SLAC_RTCM3',body=None, headers=headers)
 response = connection.getresponse()
 
+# Message format
+# https://forum.u-blox.com/index.php/16898/decoding-rtcm3-message
 def get_length(msg):
     assert( msg[0] == chr(0xd3))
     return (ord(msg[1])* 8 + ord(msg[2])) & 0x3ff
@@ -26,16 +30,40 @@ def get_length(msg):
 buf = ""
 msgs = []
 
+
+last_message = time.time()
+
+
+print("Starting")
+i = 0
+
 while 1:
+    print(i)
+    i +=1
     buf += response.read(100)
-    print(len(buf), len(msgs))
+
+
+exit()
+
+while 1:
+    if response.isclosed():
+        print "responce closed"
+        break
+    buf += response.read(100)
     length = get_length(buf)
     next_header = buf[length:].find( chr(0xd3) )
+    print(length,next_header, len(buf), len(msgs))
+    print buf[length: length + next_header].encode("hex")
     if (next_header == -1):
         print "no header found"
         continue
     msgs.append( buf[:length + next_header ] )
     buf = buf[length + next_header:]
+
+    if (last_message - time.time()) > publish_interval:
+        print(len("".join(msgs)))
+        msgs = []
+        last_message = time.time()
 
 
 
