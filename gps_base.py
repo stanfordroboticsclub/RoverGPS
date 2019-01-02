65565f459dd704623ae6e24d9a5ebfa0cbdb8453
Michal Adamkiewicz <mikh3x4@gmail.com>
2019-01-02 01:34:20 +0100

    got rtcm working ish

---
+++ gps_base.py
@@ -0,0 +1,60 @@
+
+"""
+This script interfaces with the sparkfun GPS module in Base station mode
+
+It publishes RTCM corrections on port 8290 for the rover to use
+"""
+import serial, pynmea2, time
+from UDPComms import Publisher
+
+
+pub = Publisher(8290)
+
+ser = serial.Serial("/dev/tty.usbmodem1411", timeout = 0, writeTimeout = 0)
+
+# Message format
+# https://forum.u-blox.com/index.php/16898/decoding-rtcm3-message
+def get_length(msg):
+    assert( msg[0] == chr(0xd3))
+    return (ord(msg[1])* 8 + ord(msg[2])) & 0x3ff
+
+rtcm_stream = []
+
+try:
+    while 1:
+        line = ser.readline()
+        if line == "":
+            continue
+        if line[0:2] == b'$G':
+            msg = pynmea2.parse(str(line))
+            if(msg.sentence_type == "GGA"):
+                print(msg.latitude, msg.longitude)
+                # print(repr(msg))
+                # print(ser.in_waiting, ser.out_waiting)
+                rtcm = b''.join(rtcm_stream)
+                rtcm_stream = []
+
+                # to_send = { b"lat": msg.latitude,
+                #             b"lon": msg.longitude,
+                #             b"alt": 0,
+                #             b"sats": 0,
+                #             b"rtcm": rtcm}
+                to_send = [ msg.latitude, msg.longitude, rtcm]
+                pub.send(to_send)
+
+        else:
+            rtcm_stream.append(line)
+        # elif line[0] == chr(0xd3):
+        #     length = get_length(line)
+        #     print("RTCM", length, len(line) , len(line) - length) 
+
+        # else:
+        #     print("unknown", len(line))
+
+except KeyboardInterrupt:
+    print('closing')
+    ser.close()
+    raise
+except:
+    ser.close()
+    raise
--- gps_rover.py
+++ gps_rover.py
@@ -3,28 +3,45 @@
 This script interfaces with the sparkfun GPS module in Rover mode
 
 It publishes the rovers location to port 8860
-If it recives RTCM correction on port XXXX it will also use them
+If it receives RTCM correction on port 8290 it will also use them
 """
-import serial, pynmea2, time
-from UDPComms import Publisher
+import serial, pynmea2, time, math
+from UDPComms import Publisher, Subscriber, timeout
 
 
-# pub = Publisher(8860)
+pub_gps = Publisher(8280)
+rtcm_sub = Subscriber(8290, timeout=0)
 
-ser = serial.Serial("/dev/tty.usbmodem1411", timeout = 0, writeTimeout = 0)
+ser = serial.Serial("/dev/tty.usbmodem1421", timeout = 0, writeTimeout = 0)
+
+def project(lat, lon, lat_orig, lon_orig):
+    RADIUS = 6371 * 1000
+    lon_per_deg = RADIUS * 2 * math.pi / 360
+    lat_per_deg = lon_per_deg * math.cos(math.radians(lat_orig))
+
+    x = (lon - lon_orig) * lon_per_deg
+    y = (lat - lat_orig) * lat_per_deg
+
+    return (x,y)
+
 
 try:
-while 1:
-    line = ser.readline()
-    if line[0:2] == '$G':
-        msg = pynmea2.parse(str(line))
+    while 1:
         try:
+            correction = rtcm_sub.recv()
+            print("got correction")
+        except:
+            pass
+        else:
+            ser.write(correction[2])
+
+        line = ser.readline()
+        if line[0:2] == '$G':
+            msg = pynmea2.parse(str(line))
             if(msg.sentence_type == "GGA"):
                 print(msg.latitude, msg.longitude)
                 print(repr(msg))
-                print(ser.in_waiting, ser.out_waiting)
-        except AttributeError:
-            pass
+                pub_gps.send( [msg.latitude, msg.longitude] )
 
 except KeyboardInterrupt:
     print('closing')
